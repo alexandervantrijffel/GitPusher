1c6092acc13b6439596462ea1a39940312cfcf07
Alexander van Trijffel <lexmail@gmail.com>
2016-01-31 17:29:00 +0100

    Load config from configuration.json

--- GitPusher/GitService.cs
+++ GitPusher/GitService.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using JsonConfig;
 
 namespace GitPusher
 {
+    public class ConfigurationException : Exception
+    {
+        public ConfigurationException(string message) : base(message)
+        {
+        }
+    }
+
     public class GitService : IService
     {
         private IList<DirectoryWatcher> _directoryWatchers = new List<DirectoryWatcher>(); 
@@ -13,7 +24,9 @@ namespace GitPusher
         {
             try
             {
-                var directories = new List<string>() { @"c:\data\testwatch"};
+                ;
+
+                var directories = LoadRepositoryConfig().ToList();
                 Task.Factory.StartNew(() => 
                         Parallel.ForEach<string>(directories, directory =>
                         {
@@ -22,6 +35,7 @@ namespace GitPusher
                 foreach(var directory in directories)
                     _directoryWatchers.Add(new DirectoryWatcher(directory));
             }
+
             catch (Exception)
             {
                 // todo log
@@ -29,6 +43,24 @@ namespace GitPusher
             }
 
             
+        }
+
+        private static IEnumerable<string> LoadRepositoryConfig()
+        {
+            Uri uri = new Uri(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase)));
+            dynamic config = Config.ApplyJsonFromPath(Path.Combine(uri.LocalPath, "configuration.json"));
+
+            if (!(config.Repositories is Array) || config.Repositories.Length == 0)
+            {
+                throw new ConfigurationException("No Repositories found in file configuration.json");
+            }
+
+            for (int i = 0; i < config.Repositories.Length ; i++)
+            {
+                if (string.IsNullOrEmpty(config.Repositories[i].BaseDir))
+                    throw new Exception("One or more Repositories in configuration.json have an invalid BaseDir property.");
+                yield return config.Repositories[i].BaseDir;
+            }
         }
 
         public void Stop()
